6c7f9111b6ed84fc8be46144843be2315fd15eb2
CHILKUR-DATTATHREYA <dattatreyachilkur@gmail.com>
2026-03-26 15:38:17 +0530

    Update main.py

--- server/main.py
+++ server/main.py
@@ -16,8 +16,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://context-chat-bot.vercel.app/", "http://localhost:3000"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allow_headers=["Content-Type", "Authorization", "X-User-Id", "X-Namespace"]
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 @app.middleware("http")
